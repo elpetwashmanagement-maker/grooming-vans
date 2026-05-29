3ca515e71ba3380514af56a67e712d8dcc662b13
elpetwashmanagement-maker <elpetwashmanagement@gmail.com>
2026-05-29 11:19:15 -0400

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,24 @@
+const CACHE_NAME = 'groomora-v3';
+const urlsToCache = ['/', '/index.html'];
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
